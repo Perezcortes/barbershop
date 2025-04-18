78f80edaa234ffc93b93f5ca85fdfcda4803837d
José Pérez <9531447499a@gmail.com>
2025-04-17 21:30:23 -0600

    Archivos listos

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+console.log('Conectado correctamente a la base de datos barber_db');
+
+export default connection;
